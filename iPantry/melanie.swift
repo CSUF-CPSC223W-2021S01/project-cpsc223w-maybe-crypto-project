99dc49f60f505b4a3465ffe891bcef5354f1e87a
Melanie Mach <melaniemach@csu.fullerton.edu>
2021-03-10 21:44:31 -0800

    Inventory class for inventory page

--- iPantry/melanie.swift
+++ iPantry/melanie.swift
@@ -7,4 +7,42 @@
 
 import Foundation
 
-//melanie's test file
+//melanie testing
+
+
+//create a class for Inventory
+
+class Inventory {
+    //properties
+    var itemName : String
+    var itemQuantity : Int
+    var itemCategory : String
+    
+    // could create a dictonary?
+    // need more ideas
+    //var itemList = [String, Int]
+    
+    //init
+    init() {
+        itemName = "Grass"
+        itemQuantity = 0
+        itemCategory = "Vegetables"
+    }
+    
+    //init to pass in values
+    init(itemName : String, itemQuantity : Int, itemCategory : String) {
+        self.itemName = itemName
+        self.itemQuantity = itemQuantity
+        self.itemCategory = itemCategory
+    }
+    
+    func displayQuant() -> String {
+        return "There are \(itemQuantity) \(itemName)"
+    }
+    
+    func displayCategory() -> String {
+        return "\(itemName) is a \(itemCategory) product."
+    }
+}
+
+
