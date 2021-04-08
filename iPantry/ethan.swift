d1fa226847333a447e4a0aa388b88dfa4e18a4c6
Ethan Bartlett <mr_ecb@csu.fullerton.edu>
2021-04-08 01:07:47 -0700

    Inventory Page base setup. Attempted a way to store Expiration dates.

--- iPantry/ethan.swift
+++ iPantry/ethan.swift
@@ -7,4 +7,88 @@
 
 import Foundation
 
-// ethan testing
+struct FoodData {
+    static var food_data:[String:(Int,String, UIImage?)] = [:]
+}
+
+class FoodItem {
+    var ID: String
+    var name: String
+    var timestamp: Int
+    
+    init(id: String, n: String, t: Int) {
+        ID = id
+        name = n
+        timestamp = t
+    }
+    
+    static func ==(lhs: FoodItem, rhs: FoodItem) -> Bool {
+        return (lhs.ID == rhs.ID)
+    }
+}
+
+class FoodList {
+    var name: String?
+    var contents: [FoodItem] = []
+    
+    func clear() {
+        name = nil
+        contents.removeAll()
+    }
+}
+
+class ListItem {
+    var ID: String
+    var name: String
+    
+    init(id: String, n: String) {
+        ID = id
+        name = n
+    }
+    
+    static func ==(lhs: ListItem, rhs: ListItem) -> Bool {
+        return (lhs.ID == rhs.ID)
+    }
+}
+
+class ShoppingList {
+    var name: String?
+    var contents: [ListItem] = []
+    var sharedWith: [(String, UIImage)]! = [] // possible image of food or not
+    var sharedWithSET: Set<String> = []
+}
+
+
+
+// Expiration date for grocery list attemp
+
+//       let foodAdded: String = foodName.text!
+//
+//        var daysToExpire = FoodData.food_data[foodAdded]!.0
+//
+//        let ref = Database.database().reference()
+//
+//        if daysToExpire <= 0 {
+//            daysToExpire = 10000
+//        }
+//
+//        let dateOfExpiration = Calendar.current.date(byAdding: .day, value: daysToExpire, to: Date())
+//        let timeInterval = dateOfExpiration?.timeIntervalSinceReferenceDate
+//        let doe = Int(timeInterval!)
+//
+//        //post name of food, and seconds from reference date (jan 1, 2001) that it will expire
+//        let post = ["name" : foodAdded,
+//                    "timestamp" : doe] as [String : Any]
+//
+//        ref.child("AllFoodLists/\(currentUser.shared.allFoodListID!)").childByAutoId().setValue(post)
+//
+//        if daysToExpire < 1000 {
+//            getNotificationForDay(on: dateOfExpiration!, foodName: foodAdded)
+//        }
+
+
+
+
+
+
+
