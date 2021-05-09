5ae349697da24913af5dbad144f3d6b8cc398474
Conner <connerccook@csu.fullerton.edu>
2021-05-09 15:33:49 -0700

    created table view with temporary data

--- iPantryApp/InventoryViewController.swift
+++ iPantryApp/InventoryViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class InventoryViewController: UIViewController {
-
+    
+    //temporary array that holds the items to be displayed
+    let inventory = [
+            "apple",
+            "berries",
+            "carrots"
+        ]
+    
+    @IBOutlet var inventoryView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,6 +25,10 @@ class InventoryViewController: UIViewController {
         let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeAction(swipe:)))
         leftSwipe.direction = UISwipeGestureRecognizer.Direction.left
         self.view.addGestureRecognizer(leftSwipe)
+        
+        //initiates the delegate and datasource
+        inventoryView.delegate = self
+        inventoryView.dataSource = self
     }
     
 
@@ -29,4 +42,23 @@ class InventoryViewController: UIViewController {
     }
     */
 
+}
+
+extension InventoryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("you tapped me!")
+    }
+}
+
+extension InventoryViewController: UITableViewDataSource {
+    //basically tells the controller how many rows are needed
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return inventory.count
+    }
+    //displays the items to each row 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = tableView.dequeueReusableCell(withIdentifier: "item", for: indexPath)
+        item.textLabel?.text = inventory[indexPath.row]
+        return item
+    }
 }
