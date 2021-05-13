b3e4eed7815ae8fff45fa9da02b6f523bc7a2b52
Conner <connerccook@csu.fullerton.edu>
2021-05-12 23:53:06 -0700

    blah blah

--- iPantryApp/InventoryViewController.swift
+++ iPantryApp/InventoryViewController.swift
@@ -8,68 +8,36 @@
 import UIKit
 
 class InventoryViewController: UIViewController {
-
-    
-    //temporary array that holds the items to be displayed
-//    var inventory = [
-//            "apple",
-//            "berries",
-//            "carrots",
-//        ]
-    var inventory: Inventory!
-    
+    var pantry: Inventory!
     @IBOutlet var inventoryView: UITableView!
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(didGetNotification(_:)), name: NSNotification.Name("text"), object: nil)
-//        swipe left gesture to camera page
-//        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeAction(swipe:)))
-//        leftSwipe.direction = UISwipeGestureRecognizer.Direction.left
-//        self.view.addGestureRecognizer(leftSwipe)
 //
 //        initiates the delegate and datasource
-        inventory = Inventory()
+        pantry = Inventory()
         inventoryView.delegate = self
         inventoryView.dataSource = self
     }
     
     @objc func didGetNotification(_ notification: Notification) {
         let text = notification.object as! String?
-//        inventory.append(text!)
-//        for blah in inventory {
-//            print(blah)
-//        }
-        inventory.inventory.items.append(text!)
-        inventory.save()
-        for item in inventory.inventory.items {
-            print(item)
-        }
-        print(text!)
+        pantry.inventory.items.append(text!)
+        pantry.save()
     }
 
     // swipe left gesture to remove inventory items from table view.
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            inventory.inventory.items.remove(at: indexPath.row)
+            pantry.inventory.items.remove(at: indexPath.row)
+            pantry.save()
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
         
         }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension InventoryViewController: UITableViewDelegate {
@@ -81,46 +49,14 @@ extension InventoryViewController: UITableViewDelegate {
 extension InventoryViewController: UITableViewDataSource {
     // Tells the controller how many rows are needed
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return inventory.inventory.items.count
+        return pantry.inventory.items.count
     }
     
     // Displays the items to each row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let item = tableView.dequeueReusableCell(withIdentifier: "item", for: indexPath)
-        item.textLabel?.text = inventory.inventory.items[indexPath.row]
+        item.textLabel?.text = pantry.inventory.items[indexPath.row]
         return item
     }
 }
 
-// Save Temp array data function.
-func saveInventory() {
-    _ = saveInventory
-    
-    _ = saveInventory
-    
-    _ = saveInventory
-    
-    let items = ["apple", "berries", "carrots"]
-    
-    // Gives access to Documents Directory, enables read and write files in that directory
-    // Save a collection to a file and loaded the data back from file back into our program
-    let documentsDirectory =
-      FileManager.default.urls(for: .documentDirectory,
-      in: .userDomainMask).first!
-    let archiveURL =
-      documentsDirectory.appendingPathComponent("Inventory_test")
-        .appendingPathExtension("plist")
-    
-    let propertyListEncoder = PropertyListEncoder()
-    let encodedInventory = try? propertyListEncoder.encode(items)
-    try? encodedInventory?.write(to: archiveURL,
-      options: .noFileProtection)
-    
-    let propertyListDecoder = PropertyListDecoder()
-    if let retrievedInventoryData = try? Data(contentsOf: archiveURL),
-        let decodedInventory = try?
-          propertyListDecoder.decode(Array<String>.self, from:
-          retrievedInventoryData) {
-        print(decodedInventory)
-    }
-}
